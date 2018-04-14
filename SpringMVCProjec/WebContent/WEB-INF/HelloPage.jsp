35ff267afe9d657d30360c092ce1fa1f19b7a7b5
princejvm <sksinha.c@gmail.com>
2018-04-14 19:07:57 +0530

    first mvc project

---
+++ SpringMVCProjec/WebContent/WEB-INF/HelloPage.jsp
@@ -0,0 +1,9 @@
+
+<html>
+
+</head>
+<body>
+	<h1>First Spring MVC project</h1>
+	<h2>${welcomeMessage}</h2>
+</body>
+</html>
---
+++ SpringMVCProjec/src/hellocontroller/HelloController.java
@@ -0,0 +1,19 @@
+package hellocontroller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		ModelAndView modelAndView = new ModelAndView("HelloPage");
+		modelAndView.addObject("welcomeMessage","Hi! This is the first MVC Project");
+		
+		return modelAndView;
+	}
+}
